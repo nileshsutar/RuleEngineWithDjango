2eeb7857a2bbcc3aa03b84a632aac641b2de5cda
Nilesh Sutar <sutar.nilesh@gmail.com>
2016-10-07 13:01:56 +0530

    removed directory

--- qsiruleengine/qsiruleengine/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .qsicelery import app as celery_app
-
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-
--- qsiruleengine/ruleengine/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class RuleengineConfig(AppConfig):
-    name = 'ruleengine'
